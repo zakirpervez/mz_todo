deda18265f64a5647b6f967cd594c80302c05c4b
zakirpervez <zakir.next@gmail.com>
2024-08-24 16:03:35 +0530

    App_Architecture(Clean Architecture): Added view model dependency tck-005

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -38,7 +38,10 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- presentation/src/main/java/com/example/presentation/viewmodel/TodoViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/TodoViewModel.kt
@@ -1,7 +1,32 @@
 package com.example.presentation.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.domain.entities.TodoItem
+import com.example.domain.use_case.add_todo_item.AddTodoItemUseCase
+import com.example.domain.use_case.get_todo_item.GetTodoItemsUseCase
+import kotlinx.coroutines.launch
 
-class TodoViewModel : ViewModel() {
+class TodoViewModel(
+    private val addTodoItemUseCase: AddTodoItemUseCase,
+    private val getTodoItemsUseCase: GetTodoItemsUseCase
+) : ViewModel() {
+    var todoItems by mutableStateOf<List<TodoItem>>(emptyList())
+        private set
 
+    fun addTodoItem(text: String) {
+        viewModelScope.launch {
+            addTodoItemUseCase(TodoItem(text = text))
+            loadTodoItems()
+        }
+    }
+
+    fun loadTodoItems() {
+        viewModelScope.launch {
+            todoItems = getTodoItemsUseCase()
+        }
+    }
 }
